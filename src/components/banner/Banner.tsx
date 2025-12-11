2a00a8475bd6808365444ddf774546ce536f1125
regina.sadrieva.it@gmail.com <regina.sadrieva.it@gmail.com>
2025-12-11 19:23:10 +0900

    banner fix

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -3,15 +3,10 @@ import styles from './Banner.module.scss';
 export default function Banner() {
   return (
     <section className={styles.banner}>
-      <div className={styles.banner__content}>
-        <h1 className={styles.banner__title}>
-          Realworld Blog
-          <span className={styles.banner__underline}></span>
-        </h1>
-        <p className={styles.banner__subtitle}>
-          A place to share your knowledge.
-        </p>
-      </div>
+      <h1 className={styles.banner__title}>Realworld Blog</h1>
+      <p className={styles.banner__subtitle}>
+        A place to share your knowledge.
+      </p>
     </section>
   );
 }
